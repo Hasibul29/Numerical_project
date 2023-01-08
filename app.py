59da5d631724f83453c3ee8b8e91839471e10954
Sheikh Md. Hasibul Hasan <59611301+Hasibul29@users.noreply.github.com>
2023-01-08 14:55:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def gausselimination(A, b ,test=False):
 	col3.write("Japan")
 	st.subheader("Phone Quantity")
 	source = pd.DataFrame({
-		'Quantity': [int(x[0]), int(x[1]), int(x[2]),int((other1 + other2 + other3)/100)],
+		'Quantity': [int(x[0]), int(x[1]), int(x[2]),int((other1 + other2 + other3)/1000)],
 		'Phone': ['Iphone', 'Huawei', 'Samsung','Other']
 	})
 
